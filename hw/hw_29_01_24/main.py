c3bc2acd4e052bd167e1ec1bc8e4015ecff55755
Yerassyl <yerassyl0100@mail.ru>
2024-02-06 17:32:54 +0600

    hw_28_01_24

--- hw/hw_29_01_24/main.py
+++ hw/hw_29_01_24/main.py
@@ -3,10 +3,10 @@ import pandas as pd
 sales_2021 = pd.read_csv('sales_2021.csv')
 sales_2022 = pd.read_csv('sales_2022.csv')
 
-merged_sales = pd.merge(sales_2021, sales_2022, on='product_id', suffixes=('_2021', '_2022'))
+concat_sales = pd.concat([sales_2021, sales_2022])
 
-merged_sales['total_revenue'] = merged_sales['revenue_2021'] + merged_sales['revenue_2022']
-
-result_df = merged_sales[['product_id', 'total_revenue']]
+result_df = concat_sales.groupby('product_id').agg(
+    Total_Revenue=('revenue', 'sum')
+).reset_index()
 
 result_df.to_csv('result.csv', index=False)
